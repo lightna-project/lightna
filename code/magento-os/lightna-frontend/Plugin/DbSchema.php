fc8fd57484378d2120066287a33d169526e4c51a
Oleksandr Veselovskyi <vesomy@gmail.com>
2024-08-26 11:14:14 +0300

    Fix Magento setup:upgrade error: Cannot process definition to array for type enum. Solution: ignore lightna_* tables by Magento

---
+++ code/magento-os/lightna-frontend/Plugin/DbSchema.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightna\Frontend\Plugin;
+
+use Magento\Framework\Setup\Declaration\Schema\Db\DbSchemaReaderInterface;
+
+class DbSchema
+{
+    public function afterReadTables(DbSchemaReaderInterface $subject, array $tables): array
+    {
+        foreach ($tables as $key => $table) {
+            if (str_starts_with($table, 'lightna_')) {
+                unset($tables[$key]);
+            }
+        }
+
+        return $tables;
+    }
+}
