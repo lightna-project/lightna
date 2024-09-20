56caae46095fc79784ecfd144ca7392e36985d42
Valentyna <valentyna.horpynych@gmail.com>
2024-09-20 22:02:52 +0300

    Use one config file for webpack

--- entry/magento-os/webpack.config.js
+++ entry/magento-os/webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
 
-module.exports = {
+const config = {
     entry: '../../code/lightna/magento-os-frontend/js/index.js',
     output: {
         filename: 'common.js',
         path: path.resolve(__dirname, '../../project/magento-os/pub/static/lightna/compiled/js'),
     },
     mode: 'production'
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'inline-source-map';
+        config.watch = true;
+    }
+
+    return config;
 };
