56e34156dcaef4ee5c95c8b52efe30fb9831e899
Valentyna <valentyna.horpynych@gmail.com>
2024-09-25 19:47:09 +0300

    Clean-up js: removed scope and options

--- code/lightna/magento-os-frontend/js/common/Collapsible.js
+++ code/lightna/magento-os-frontend/js/common/Collapsible.js
@@ -4,17 +4,15 @@ import { isElementInViewport } from '../lib/utils/isElementInViewport';
 export class Collapsible {
     items = [];
 
-    constructor(scope, options = {}) {
-        this.scope = scope;
-        this.options = options;
+    constructor() {
         this.init();
         this.bindEvents();
     }
 
     init() {
-        const jsc = $$('.cjs-collapsible', this.scope);
-        jsc.forEach((item) => {
-            const trigger = item.querySelector('.cjs-collapsible-trigger');
+        const cjs = $$('.cjs-collapsible');
+        cjs.forEach((item) => {
+            const trigger = item.querySelector('[data-action="toggle-collapsible"]');
             if (trigger) {
                 this.items.push({
                     container: item,
@@ -33,13 +31,6 @@ export class Collapsible {
     }
 
     onToggle(item) {
-        if (this.options.type === 'accordion') {
-            this.items.forEach((i) => {
-                if (i !== item) {
-                    i.container.classList.remove('collapsible--active');
-                }
-            });
-        }
         item.container.classList.toggle('collapsible--active');
         this.scrollIfNotVisible(item.trigger);
     }
