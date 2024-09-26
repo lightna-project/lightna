e0f96f1fedd6d59273d546d192a7ac7c268333bc
Oleksandr Veselovskyi <vesomy@gmail.com>
2024-09-26 20:29:43 +0300

    Webpack modularity

--- code/lightna/magento-os-frontend/js/index-semi.js
+++
@@ -1,7 +0,0 @@
-import { ShoppingCart } from './common/ShoppingCart';
-import { Menu } from './common/Menu';
-import { MobileOverlay } from './common/MobileOverlay';
-
-new ShoppingCart();
-new Menu();
-new MobileOverlay();
--- entry/magento-os/config.php
+++ entry/magento-os/config.php
@@ -14,6 +14,7 @@ return [
         'Lightna\Redis' => '../../code/lightna/lightna-redis',
         'Lightna\Session' => '../../code/lightna/lightna-session',
         'Lightna\Elasticsearch' => '../../code/lightna/lightna-elasticsearch',
+        'Lightna\Webpack' => '../../code/lightna/lightna-webpack',
         'Lightna\Tailwind' => '../../code/lightna/lightna-tailwind',
         'Lightna\Magento' => '../../code/lightna/magento-os-backend',
         'Lightna\Magento\Frontend' => '../../code/lightna/magento-os-frontend',
