6829d65de1503250420445aa99416e7a8a2c87ae
Valentyna <valentyna.horpynych@gmail.com>
2024-10-19 20:46:40 +0300

    Possibility to extend HttpClient

--- code/lightna/lightna-engine/js/lib/HttpClient.js
+++ code/lightna/lightna-engine/js/lib/HttpClient.js
@@ -1,44 +1,60 @@
 import { FormKey } from 'lightna/lightna-engine/lib/FormKey';
 import { objectToQuery } from 'lightna/lightna-engine/lib/utils/objectToQuery';
 
-class HttpClient {
-    headers = {
+export class HttpClient {
+    static headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'X-Request-With': 'Lightna',
     };
-    lock = false;
+    static _lock = null;
 
-    constructor(options = {}) {
-        this.headers = { ...this.headers, ...options.headers };
+    static get(url, options = {}) {
+        return this.fetch(url, {
+            ...options,
+            method: 'GET',
+        });
     }
 
-    async fetch(url, options = {}) {
-        if (options.top && this.lock) {
+    static post(url, data, options = {}) {
+        if (!data.form_key) {
+            data.form_key = FormKey.get();
+        }
+
+        return this.fetch(url, {
+            ...options,
+            body: objectToQuery(data),
+            method: 'POST',
+        });
+    }
+
+    static async fetch(url, options = {}) {
+        if (options.top && this._lock) {
             throw new Error(
                 `HttpClient: Can't send new top level request for "${url}" until no response from previous`,
             );
         }
 
-        const onSuccess = options.onSuccess ? options.onSuccess : this.onSuccess;
-        const onError = options.onError ? options.onError : this.onError;
-        const headers = { ...this.headers, ...options.headers };
+        const onSuccess = options.onSuccess ? options.onSuccess : this._onSuccess;
+        const onError = options.onError ? options.onError : this._onError;
+        options.headers = { ...this.headers, ...options.headers };
 
         try {
-            const response = await fetch(url, {
+            this._lock = fetch(url, {
                 ...options,
-                headers: headers,
+                headers: options.headers,
             });
 
-            this.lock = false;
+            const response = await this._lock;
+            this._lock = null;
 
-            return await this.handleJson(response, onSuccess, onError);
+            return await this._handleJson(response, onSuccess, onError);
         } catch (error) {
-            this.lock = false;
+            this._lock = null;
             onError(error);
         }
     }
 
-    async handleJson(response, onSuccess, onError) {
+    static async _handleJson(response, onSuccess, onError) {
         if (response.headers.get('Content-Type') !== 'application/json') {
             throw new Error('JSON expected');
         }
@@ -68,33 +84,10 @@ class HttpClient {
         return responseJson;
     }
 
-    get(url, options = {}) {
-        return this.fetch(url, {
-            ...options,
-            method: 'GET',
-        });
+    static _onSuccess(response) {
     }
 
-    post(url, data, options = {}) {
-        if (!data.form_key) {
-            data.form_key = FormKey.get();
-        }
-
-        return this.fetch(url, {
-            ...options,
-            body: objectToQuery(data),
-            method: 'POST',
-        });
-    }
-
-    onSuccess(response) {
-    }
-
-    onError(error) {
+    static _onError(error) {
         throw error;
     }
 }
-
-const request = new HttpClient();
-
-export default request;
