9df471ad777d92c08a4f44799d923533e08260e9
Valentyna <valentyna.horpynych@gmail.com>
2024-10-20 17:42:24 +0300

    Fixed cart opening on lane pages

--- code/lightna/magento-os-frontend/js/common/PageMessage.js
+++ code/lightna/magento-os-frontend/js/common/PageMessage.js
@@ -34,6 +34,8 @@ export class PageMessage {
     }
 
     static clearAll() {
-        this.container.innerHTML = '';
+        if (this.container) {
+            this.container.innerHTML = '';
+        }
     }
 }
