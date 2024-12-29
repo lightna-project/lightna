11664a8916750d35a4fae8a85a500efe3e27442f
Oleksandr Veselovskyi <vesomy@gmail.com>
2024-12-29 23:19:59 +0200

    Remove const type to keep PHP 8.1 support. Improve Lightna Lane initialization.

--- code/lightna/magento-os-backend/App/Entity/Config.php
+++ code/lightna/magento-os-backend/App/Entity/Config.php
@@ -8,8 +8,8 @@ use Lightna\Engine\App\Entity\EntityA;
 
 class Config extends EntityA
 {
-    public const string NAME = 'config';
-    public const string STORAGE_PREFIX = 'CFG_';
+    public const NAME = 'config';
+    public const STORAGE_PREFIX = 'CFG_';
 
     /** @AppConfig(entity/config/storage) */
     protected string $storageName;
