ba0b55c986594b648489ad9d14ed659211b1279c
Valentyna <valentyna.horpynych@gmail.com>
2025-01-06 19:33:59 +0200

    TEST

---
+++ code/lightna/magento-os-frontend/Data/Block.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightna\Magento\Frontend\Data;
+class Block extends \Lightna\Engine\Data\Block
+{
+    public function hiThere(): string
+    {
+        return 'hi there!';
+    }
+}
