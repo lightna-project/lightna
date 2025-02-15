c6fc13fad08ec140270cdd680c065fb72ac1de07
Valentyna <valentyna.horpynych@gmail.com>
2025-02-15 13:59:43 +0200

    Refactor Menu.js

--- code/lightna/magento-frontend/js/common/Menu.js
+++ code/lightna/magento-frontend/js/common/Menu.js
@@ -9,129 +9,106 @@ export class Menu {
     mobileBreakpoint = 768;
     mouseEnterTimeout = null;
     mouseEnterBlocked = false;
-    classNames = {
+    classes = {
         activeItem: 'active',
         activeOverlay: 'overlay-active',
         hoverItem: 'hover',
         preventClick: 'prevent-click',
+        noJs: 'no-js',
     };
     selectors = {
         itemTopLevelTitle: '.level-1__title',
-        itemsWithChildren: '.level-1.has-children .level-1__title',
-        itemsTopLevel: '.level-1',
+        itemWithChildren: '.level-1.has-children .level-1__title',
+        itemTopLevel: '.level-1',
         hasChildren: '.has-children',
         menuContainer: '.main-navigation',
     };
 
     constructor(options = {}) {
-        this.delay = isTouchDevice() ? 0 : options.delay || 150;
         this.menuContainer = $(this.selectors.menuContainer);
-        this.itemsWithChildren = $$(this.selectors.itemsWithChildren);
-        this.itemsTopLevel = $$(this.selectors.itemsTopLevel);
         if (!this.menuContainer) return;
-        this.menuContainer.classList.remove('no-js');
-        this.bindEvents();
+
+        this.delay = isTouchDevice() ? 0 : options.delay || 150;
+        this.itemsWithChildren = $$(this.selectors.itemWithChildren);
+        this.itemsTopLevel = $$(this.selectors.itemTopLevel);
+        
+        this.menuContainer.classList.remove(this.classes.noJs);
+        this.initializeEventListeners();
     }
 
-    bindEvents() {
-        document.addEventListener(
-          'mouseenter',
-          this.preventMenuActivation.bind(this),
-          { once: true },
-        );
-
-        this.itemsWithChildren.forEach((item) => {
-            item.addEventListener(
-              'click',
-              this.itemWithChildrenClick.bind(this, item),
-            );
+    initializeEventListeners() {
+        document.addEventListener('mouseenter', this.preventMenuActivation.bind(this), { once: true });
+        this.itemsWithChildren.forEach(item => {
+            item.addEventListener('click', this.onItemWithChildrenClick.bind(this, item));
         });
 
-        this.itemsTopLevel.forEach((item) => {
-            item.addEventListener(
-              'mouseenter',
-              this.itemTopLevelMouseEnter.bind(this, item),
-            );
-            item.addEventListener(
-              'mouseleave',
-              this.itemTopLevelMouseLeave.bind(this, item),
-            );
+        this.itemsTopLevel.forEach(item => {
+            item.addEventListener('mouseenter', this.onItemTopLevelMouseEnter.bind(this, item), { passive: true });
+            item.addEventListener('mouseleave', this.onItemTopLevelMouseLeave.bind(this, item), { passive: true });
         });
     }
 
     preventMenuActivation() {
         if (isTouchDevice()) return;
-
-        this.itemsTopLevel.forEach((item) => {
-            if (item.matches(':hover')) {
-                this.mouseEnterBlocked = true;
-            }
-        });
+        this.mouseEnterBlocked = this.menuContainer.matches(':hover');
     }
 
-    itemWithChildrenClick(item, event) {
+    onItemWithChildrenClick(item, event) {
         if (window.innerWidth < this.mobileBreakpoint) {
             this.toggleMobileSubmenu(item);
             event.preventDefault();
             return;
         }
 
-        if (item.matches(`.${this.classNames.preventClick}`)) {
+        if (item.matches(`.${this.classes.preventClick}`)) {
             event.preventDefault();
-            item.classList.remove(this.classNames.preventClick);
+            item.classList.remove(this.classes.preventClick);
         }
     }
 
-    itemTopLevelMouseEnter(item) {
+    onItemTopLevelMouseEnter(item) {
         if (this.mouseEnterBlocked) return;
 
         if (isTouchDevice()) {
-            $(this.selectors.itemTopLevelTitle, item).classList.add(
-              this.classNames.preventClick,
-            );
+            $(this.selectors.itemTopLevelTitle, item).classList.add(this.classes.preventClick);
         }
 
         this.mouseEnterTimeout = setTimeout(() => {
-            const activeItem = $(
-              `.${this.classNames.hoverItem}`,
-              this.menuContainer,
-            );
-            activeItem?.classList.remove(this.classNames.hoverItem);
-            item.classList.add(this.classNames.hoverItem);
+            const activeItem = $(`.${this.classes.hoverItem}`, this.menuContainer);
+            activeItem?.classList.remove(this.classes.hoverItem);
+            item.classList.add(this.classes.hoverItem);
             this.toggleOverlay(true, item.matches(this.selectors.hasChildren));
         }, this.delay);
     }
 
-    itemTopLevelMouseLeave(item) {
+    onItemTopLevelMouseLeave(item) {
         this.mouseEnterBlocked = false;
         clearTimeout(this.mouseEnterTimeout);
+
         const isPrevHover = item.previousElementSibling?.matches(':hover');
         const isNextHover = item.nextElementSibling?.matches(':hover');
-
         if (isPrevHover || isNextHover) {
             return;
         }
 
         setTimeout(() => {
-            const currentHover = $(
-              `.${this.classNames.hoverItem}`,
-              this.menuContainer,
-            );
-            if (!currentHover) return;
-
-            currentHover.classList.remove(this.classNames.hoverItem);
-            this.toggleOverlay(false);
+            if (item.matches(':hover')) return;
+            const currentHover = $(`.${this.classes.hoverItem}`, this.menuContainer);
+            if (currentHover) {
+                currentHover.classList.remove(this.classes.hoverItem);
+                this.toggleOverlay(false);
+            }
         }, this.delay);
     }
 
     toggleMobileSubmenu(item) {
-        item.parentNode.classList.toggle(this.classNames.activeItem);
+        item.parentNode.classList.toggle(this.classes.activeItem);
     }
 
     toggleOverlay(show, hasChildren = false) {
-        document.body.classList.toggle(
-          this.classNames.activeOverlay,
-          show && hasChildren,
-        );
+        const shouldShow = show && hasChildren;
+        if (document.body.classList.contains(this.classes.activeOverlay) !== shouldShow) {
+            document.body.classList.toggle(this.classes.activeOverlay, shouldShow);
+        }
     }
 }
