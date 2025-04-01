dcb0e8f679a9c1ec469ff292830267547b1a003a
Valentyna <valentyna.horpynych@gmail.com>
2025-04-01 18:20:09 +0300

    Lazy load for related products

---
+++ code/lightna/magento-frontend/js/common/LazyContent.js
@@ -0,0 +1,41 @@
+import { $$ } from 'lightna/engine/lib/utils/dom';
+import { Blocks } from 'lightna/engine/lib/Blocks';
+
+export class LazyContent {
+    static DATA_ATTR = 'data-lazy-block-id';
+    component = '.cjs-lazy';
+
+    constructor() {
+        this.extendProperties();
+        this.initializeObservers();
+    }
+
+    extendProperties() {
+    }
+
+    initializeObservers() {
+        const observer = new IntersectionObserver(async (entries, observer) => {
+            await this.handleIntersection(entries, observer);
+        });
+
+        this.getSections().forEach(section => observer.observe(section));
+    }
+
+    async handleIntersection(entries, observer) {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                await this.load(entry.target);
+                observer.unobserve(entry.target);
+            }
+        }
+    }
+
+    getSections() {
+        return $$(this.component);
+    }
+
+    async load(section) {
+        const blockId = section.getAttribute(LazyContent.DATA_ATTR);
+        await Blocks.updateHtml([blockId]);
+    }
+}
